87a91664417ec3be62926192c091e12ac89a3117
Geoffrey Gao <geoffreygao1@gmail.com>
2023-03-09 10:23:47 -0800

    Created Client Model

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using MySqlConnector;
+
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set; }
+  }
+}
