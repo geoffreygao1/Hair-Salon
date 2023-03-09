f5f93bdba24995ea6a41ccd1b3c71a0b755e6064
Geoffrey Gao <geoffreygao1@gmail.com>
2023-03-09 15:15:25 -0800

    Added post route for create

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -27,5 +27,12 @@ namespace HairSalon.Controllers
       return View();
     }
 
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
